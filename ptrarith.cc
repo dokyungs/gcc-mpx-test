12af50cd2e45312dc556901538fb198e4214b265
Dokyung Song <dokyung.song@gmail.com>
2016-12-05 09:46:23 -0800

    Fix pointer arithmetic test case

--- ptrarith.cc
+++ ptrarith.cc
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  int a = 1;
-  int* pA = &a;
+  int a[] = {1,3,7,15,31};
+  int* pA = a;
   int** ppA = &pA;
 
   printf("%d\n", **ppA);
+  printf("%lx\n", (unsigned long)*ppA);
 
-  //*ppA += 7;
+  *ppA += 1;
+  printf("%d\n", **ppA);
+  printf("%lx\n", (unsigned long)*ppA);
 }
